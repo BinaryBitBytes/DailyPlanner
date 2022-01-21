58c4b7ae3326c88b45f761a87c5c32b991a7b2c3
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-01-20 22:43:11 -0500

    updated javascript, css, and html. Still need to refine all code.

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+console.log(month.length);
+var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+console.log(dayOfWeek.length);
+var  passedDueTask = [];
+console.log(passedDueTask.length);
+var currentTask =[];
+console.log(currentTask.length);
+var completedTask = [];
+console.log(completedTask.length);
+var highlightDue = (x <= 3);
+
+var complete = true;
+var pending = false;
+
+var createTask = make;
+var checkTaskComplete = check;
+
+function checkTask() {
+    if task === passedDueTask {
+        window.alert('These tasks are passed due: ' + passedDueTask[]);
+    }
+    else if task === currentTask {
+        window.alert('These tasks are pending: ' + currentTask[]);
+    }
+    else task === completedTask {
+        window.alert('These tasks are completed: '+ completedTask[]);
+    }
+
+}
+
+/*
+//checks if expression is true/ using &&=BOTH
+if  && highlightDue {
+
+// If not met then next checks inside () of else if, to prove if TRUE
+} else if () {
+
+// If not met then evaluates then returns false in else statement
+} else {
+
+}
+
+//use for-loop to execute more than once to print completed tasks
+for (var i=0; i < completedTask.length; i++) {
+    console.log("These are the completed tasks: "+ completedTask[i] + ".");
+}
+*/
+
